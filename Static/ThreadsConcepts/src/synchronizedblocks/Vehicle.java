c7aa5e18c9e576ec30f737bd5453dbf6a699e12a
sravani <sravania2002@gmail.com>
2024-03-26 16:40:04 +0530

    signal red

---
+++ Static/ThreadsConcepts/src/synchronizedblocks/Vehicle.java
@@ -0,0 +1,25 @@
+package synchronizedblocks;
+
+
+
+public class Vehicle extends Thread{
+    int liters;
+    String status;
+    String type;
+    PetrolStation1 petrolStation;
+    public Vehicle(int liters, String status, String type, PetrolStation1 petrolStation) {
+        this.liters = liters;
+        this.status = status;
+        this.type = type;
+        this.petrolStation = petrolStation;
+    }
+    public  void run(){
+        if(type.equals("petrol")){
+            System.out.println("Vehicle Number"+Thread.currentThread().getName());
+            petrolStation.petrolsynchronizedblock(Thread.currentThread().getName(), this.liters);
+        }else{
+            petrolStation.dieselsynchronizedblock(Thread.currentThread().getName(), this.liters);
+        }
+
+    }
+}
